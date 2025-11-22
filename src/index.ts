bfa40f8f9c679b9477867bf2eeb989055a166a88
Julien Capellari <julien.capellari@gmail.com>
2025-11-22 14:33:13 +0100

    Add workload$, job$ and scheduler$ (#6)
    * feat: copy workload & job code
    
    * feat: copy workload & job tests
    
    * fix: unit tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+export * from './job$.js';
+export * from './scheduler$.js';
+export * from './unscheduler$.js';
+export * from './workload$.js';
+
 export * from './enums/workload-state.js';
---
+++ tests/unit/utils/dependencies-of.test.ts
@@ -0,0 +1,26 @@
+import { job$, workload$ } from '@/src/index.js';
+import { dependenciesOf } from '@/src/utils/dependencies-of.js';
+import { collect$, pipe$ } from 'kyrielle';
+import { describe, expect, it, vi } from 'vitest';
+
+describe('dependenciesOf', () => {
+  it('should return iterator on all node\'s dependencies', () => {
+    const nodeA = workload$({ label: 'nodeA', type: 'test', onStart: vi.fn() });
+    const nodeB = workload$({ label: 'nodeB', type: 'test', onStart: vi.fn() });
+    const nodeC = job$({ label: 'nodeC', onStart: vi.fn() });
+    const root = job$({ label: 'root', onStart: vi.fn() });
+
+    nodeC.dependsOn(nodeA);
+    nodeC.dependsOn(nodeB);
+
+    root.dependsOn(nodeA);
+    root.dependsOn(nodeC);
+
+    const result = pipe$(dependenciesOf(root), collect$());
+
+    expect(result).toHaveLength(3);
+    expect(result).toContain(nodeA);
+    expect(result).toContain(nodeB);
+    expect(result).toContain(nodeC);
+  });
+});
